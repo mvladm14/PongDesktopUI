0395c90c7e678afea95be2df2cbaf9941f97679e
mvladm14 <vlad.mirel@outlook.com>
2015-05-29 17:32:10 +0200

    pom modified and models added

---
+++ src/main/java/PongGame_UI/models/PongBall.java
@@ -0,0 +1,11 @@
+package PongGame_UI.models;
+
+public class PongBall {
+	
+    private int ballX = 250;
+    private int ballY = 250;
+    private int diameter = 20;
+    private int ballDeltaX = -1;
+    private int ballDeltaY = 3;
+
+}
