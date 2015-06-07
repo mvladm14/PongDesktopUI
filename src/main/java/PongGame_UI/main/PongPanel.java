d43d2cee6f64c7e34fa7003be9a2f3820c59383c
mvladm14 <vlad.mirel@outlook.com>
2015-06-07 21:55:04 +0200

    player 1 can now hit

--- src/main/java/PongGame_UI/main/PongPanel.java
+++ src/main/java/PongGame_UI/main/PongPanel.java
@@ -11,20 +11,32 @@ import java.awt.event.KeyListener;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
+import restInterfaces.PlayerSvcApi;
 import restInterfaces.PongBallSvcApi;
 import retrofit.RestAdapter;
-import models.Coordinates;
+import models.BallCoordinates;
+import models.PhoneCoordinates;
 import models.PongBall;
+import models.Screen;
 
 @SuppressWarnings("serial")
 public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
+	private Screen screen;
+
 	private PongBall ball;
 
+	private boolean player1CanHit = true;
+	private boolean player2CanHit = false;
+
+	// private static final String SERVER =
+	// "http://131.254.101.102:8080/myriads";
 	private static final String SERVER = "http://131.254.101.102:8080/PongServerSide";
 
 	private PongBallSvcApi pongBallSvc = new RestAdapter.Builder()
 			.setEndpoint(SERVER).build().create(PongBallSvcApi.class);
+	private PlayerSvcApi playerSvc = new RestAdapter.Builder()
+			.setEndpoint(SERVER).build().create(PlayerSvcApi.class);
 
 	private boolean showTitleScreen = true;
 	private boolean playing = false;
@@ -34,16 +46,16 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	private int ballDeltaY = 3;
 
 	private int playerOneX;
-	private int playerOneY = 250;
 	private int playerOneWidth = 10;
-	private int playerOneHeight = 50;
 
 	private int playerTwoX;
-	private int playerTwoY = 250;
-	private int playerTwoHeight = 50;
 
 	private int playerOneScore = 0;
 	private int playerTwoScore = 0;
+
+	private boolean ballCanBeHit = true;
+
+	private boolean ballCanBeHit2 = true;
 
 	// construct a PongPanel
 	public PongPanel() {
@@ -68,11 +80,14 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	}
 
 	private void initializeNonUIfields() {
+
+		screen = new Screen();
+
 		int ballX = 250;
 		int ballY = 250;
 		int diameter = 20;
 
-		Coordinates coordinates = Coordinates.create().withX(ballX)
+		BallCoordinates coordinates = BallCoordinates.create().withX(ballX)
 				.withY(ballY).build();
 
 		ball = PongBall.create().withId(1).withDiameter(diameter)
@@ -98,66 +113,103 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 			int nextBallBottom = ball.getCoordinates().getY()
 					+ ball.getDiameter() + ballDeltaY;
 
-			int playerOneRight = playerOneX + playerOneWidth;
-			int playerOneTop = playerOneY;
-			int playerOneBottom = playerOneY + playerOneHeight;
-
-			float playerTwoLeft = playerTwoX;
-			float playerTwoTop = playerTwoY;
-			float playerTwoBottom = playerTwoY + playerTwoHeight;
-
 			// ball bounces off top and bottom of screen
 			if (nextBallTop < 0 || nextBallBottom > getHeight()) {
 				ballDeltaY *= -1;
 			}
 
-			// will the ball go off the left side?
-			if (nextBallLeft < playerOneRight) {
-				// is it going to miss the paddle?
-				if (nextBallTop > playerOneBottom
-						|| nextBallBottom < playerOneTop) {
+			// has the ball entered the area where it can be hit
+			// by the first (left sided) player?
+			if (nextBallLeft < playerOneX) {
+				// ball has entered the area where it can be hit
+				leftPlayerHitBall();
 
+				// missed the paddle?
+				if (nextBallLeft <= 0) {
 					playerTwoScore++;
-
 					ball.getCoordinates().setX(250);
 					ball.getCoordinates().setY(250);
-				} else {
-					ballDeltaX *= -1;
 				}
+			} else {
+				ballCanBeHit = true;
 			}
 
-			// will the ball go off the right side?
-			if (nextBallRight > playerTwoLeft) {
-				// is it going to miss the paddle?
-				if (nextBallTop > playerTwoBottom
-						|| nextBallBottom < playerTwoTop) {
+			// has the ball entered the area where it can be hit
+			// by the second (right sided) player?
+			if (nextBallRight > playerTwoX) {
+				// ball has entered the area where it can be hit
+				rightPlayerHitBall();
 
+				// missed the paddle?
+				if (nextBallRight >= screen.getWidth()) {
 					playerOneScore++;
-
 					ball.getCoordinates().setX(250);
 					ball.getCoordinates().setY(250);
-				} else {
-					ballDeltaX *= -1;
 				}
+			} else {
+				ballCanBeHit2 = true;
 			}
 
-			// move the ball
-			ball.getCoordinates().setX(
-					ball.getCoordinates().getX() + ballDeltaX);
-			ball.getCoordinates().setY(
-					ball.getCoordinates().getY() + ballDeltaY);
+			moveBall();
 
-			// send ball coordinates to server
-			new Thread() {
-				public void run() {
-					pongBallSvc.setCoordinates(ball.getId(),
-							ball.getCoordinates());
-				}
-			}.start();
+			sendBallCoordinatesToServer();
 		}
 
 		// stuff has moved, tell this JPanel to repaint itself
 		repaint();
+	}
+
+	private void rightPlayerHitBall() {
+		new Thread() {
+			public void run() {
+				if (ballCanBeHit2) {
+					PhoneCoordinates phoneCoordinates = playerSvc
+							.getPhoneCoordinates(1);
+					if (phoneCoordinates.getY() > -2
+							&& phoneCoordinates.getY() < 2) {
+						if (phoneCoordinates.getZ() > 80) {
+							ballCanBeHit2 = false;
+							ballDeltaY *= -1;
+							ballDeltaX *= -1;							
+						}
+					}
+				}
+			}
+		}.start();
+		
+	}
+
+	private void leftPlayerHitBall() {
+		new Thread() {
+			public void run() {
+				if (ballCanBeHit) {
+					PhoneCoordinates phoneCoordinates = playerSvc
+							.getPhoneCoordinates(1);
+					if (phoneCoordinates.getY() > -2
+							&& phoneCoordinates.getY() < 2) {
+						if (phoneCoordinates.getZ() > 80) {
+							ballCanBeHit = false;
+							ballDeltaY *= -1;
+							ballDeltaX *= -1;							
+						}
+					}
+				}
+			}
+		}.start();
+
+	}
+
+	private void sendBallCoordinatesToServer() {
+		new Thread() {
+			public void run() {
+				pongBallSvc.setCoordinates(ball.getId(), ball.getCoordinates());
+			}
+		}.start();
+	}
+
+	private void moveBall() {
+		ball.getCoordinates().setX(ball.getCoordinates().getX() + ballDeltaX);
+		ball.getCoordinates().setY(ball.getCoordinates().getY() + ballDeltaY);
 	}
 
 	// paint the game screen
@@ -165,21 +217,22 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 		super.paintComponent(g);
 		g.setColor(Color.WHITE);
 
-		int width = (int) g.getClipBounds().getWidth();
-		int height = (int) g.getClipBounds().getHeight();
+		screen.setWidth((int) g.getClipBounds().getWidth());
+		screen.setHeight((int) g.getClipBounds().getHeight());
 
-		playerTwoX = width - width / 10;
-		playerOneX = width / 10;
+		playerTwoX = screen.getWidth() * 9 / 10;
+		playerOneX = screen.getWidth() / 10;
 
 		if (showTitleScreen) {
 
 			g.setFont(new Font(Font.DIALOG, Font.BOLD, 36));
 			g.setFont(new Font(Font.DIALOG, Font.BOLD, 36));
-			g.drawString("Pong", width / 2, height / 2);
+			g.drawString("Pong", screen.getWidth() / 2, screen.getHeight() / 2);
 
 			g.setFont(new Font(Font.DIALOG, Font.BOLD, 18));
 
-			g.drawString("Press 'P' to play.", width / 2, (int) height / 2 + 80);
+			g.drawString("Press 'P' to play.", screen.getWidth() / 2,
+					(int) screen.getHeight() / 2 + 80);
 
 		} else if (playing) {
 
@@ -188,7 +241,8 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
 			// draw dashed line down center
 			for (int lineY = 0; lineY < getHeight(); lineY += 50) {
-				g.drawLine(width / 2, lineY, width / 2, lineY + 25);
+				g.drawLine(screen.getWidth() / 2, lineY, screen.getWidth() / 2,
+						lineY + 25);
 			}
 
 			// draw "goal lines" on each side
@@ -197,10 +251,10 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
 			// draw the scores
 			g.setFont(new Font(Font.DIALOG, Font.BOLD, 36));
-			g.drawString(String.valueOf(playerOneScore), (int) (width / 3.5),
-					100);
-			g.drawString(String.valueOf(playerTwoScore), (int) (width / 1.5),
-					100);
+			g.drawString(String.valueOf(playerOneScore),
+					(int) (screen.getWidth() / 3.5), 100);
+			g.drawString(String.valueOf(playerTwoScore),
+					(int) (screen.getWidth() / 1.5), 100);
 
 			// draw the ball
 			g.fillOval(ball.getCoordinates().getX(), ball.getCoordinates()
@@ -239,8 +293,6 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 			if (e.getKeyCode() == KeyEvent.VK_SPACE) {
 				gameOver = false;
 				showTitleScreen = true;
-				playerOneY = 250;
-				playerTwoY = 250;
 				ball.getCoordinates().setX(250);
 				ball.getCoordinates().setY(250);
 				playerOneScore = 0;
